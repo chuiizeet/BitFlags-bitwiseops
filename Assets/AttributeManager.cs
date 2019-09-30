c0f063c642fe29a5c6ba66b9a9f68f7908b315ef
chuiizeet <chuy.guerra19@outlook.es>
2019-09-30 02:44:29 -0500

    Add walls <no gitignore boOooYZ>

--- Assets/AttributeManager.cs
+++ Assets/AttributeManager.cs
@@ -6,8 +6,39 @@ using System;
 
 public class AttributeManager : MonoBehaviour
 {
+    static public int MAGIC = 16;
+    static public int INTELLIGENCE = 8;
+    static public int CHARISMA = 4;
+    static public int FLY = 2;
+    static public int INVISIBLE = 1;
     public Text attributeDisplay;
     int attributes = 0;
+
+    
+    private void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.tag == "MAGIC") 
+        {
+            attributes |= MAGIC;
+        } 
+        else if (other.gameObject.tag == "INTELLIGENCE") 
+        {
+            attributes |= INTELLIGENCE;
+        }
+        else if (other.gameObject.tag == "CHARISMA")
+        {
+            attributes |= CHARISMA;
+        }
+        else if (other.gameObject.tag == "FLY")
+        {
+            attributes |= FLY;
+        }
+        else if (other.gameObject.tag == "INVISIBLE")
+        {
+            attributes |= INVISIBLE;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
